73d6af0ba981680a8b779101d4af90b4e3a67f71
Benia Morgan <bmorganware@gmail.com>
2024-08-05 23:52:43 -0400

    start of project

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Welcome to the Coin flip challenge!");
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Welcome {name}! Do you want to do the coin flip challenge? Yes/No");
+
+string userResponse = Console.ReadLine();
+var modifiedResponse = userResponse.ToLower();
+
+while (modifiedResponse != "yes" && modifiedResponse != "no")
+{
+    Console.WriteLine("You've entered an invalid entry. Please enter yes or no.");
+    userResponse = Console.ReadLine();
+    modifiedResponse = userResponse;
+}
+
+
+if (modifiedResponse == "no")
+{
+    Console.Write($"You are a coward {name}.");
+} else  {
+    Console.WriteLine("Heads or Tails?");
+    Console.ReadLine();
+}
+
+
 
